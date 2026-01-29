550a0583ae6905da8e42e6d801a969971b7a070c
Mohammad Danish <danish.sab05@gmail.com>
2026-01-29 23:02:53 +0530

    question 1 loop

--- LOOP/q1.cpp
+++ LOOP/q1.cpp
@@ -1,8 +1,13 @@
+//Write a program in C++ to find the first 10 natural numbers.
+
 #include <iostream>
 using namespace std;
 
 int main() {
-
-
+    int num;
+    for (int num = 0; num < 10; num++)
+    {
+        cout<<"First 10 natural number :" << num << endl;
+    }
     return 0;
 }
